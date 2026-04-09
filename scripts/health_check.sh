5de9b0b0bf9daba39352e3ed7640e084b3278a0e
Intizor <intizorpayrav@gmail.com>
2026-04-09 04:55:06 +0000

    Update gitignore and health check script

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-echo "=== Server Status ==="
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+
+
+echo -e "${YELLOW}=== Server Status ===${NC}"
 kubectl get nodes
 
 echo ""
-echo "=== Probles Pods ==="
+echo -e "${RED}=== Problems Pods ===${NC}"
 if [ -z "$1" ]; then
 	kubectl get pods -A | grep -v "Running" | grep -v "Completed"
 else
